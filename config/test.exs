46b8a73e0f851d081a87c5770d4bc9a09ca3d5af
Chris Hicks <khandrish@gmail.com>
2016-12-03 09:19:45 +0000

    make travis build pass

--- config/test.exs
+++ config/test.exs
@@ -2,3 +2,8 @@ use Mix.Config
 
 # Print only warnings and errors during test
 config :logger, level: :debug
+
+config :exmud, Exmud.Repo,
+  username: "postgres",
+  password: "",
+  database: "exmud_test_repo"
