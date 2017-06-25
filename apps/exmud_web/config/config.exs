2b88a0ba68d48e8e5e0cf147a343a4082b87bdaf
Chris Hicks <khandrish@gmail.com>
2017-06-25 06:25:19 +0000

    make server work for cloud9; update gen config

--- apps/exmud_web/config/config.exs
+++ apps/exmud_web/config/config.exs
@@ -17,6 +17,10 @@ config :exmud_web, Exmud.Web.Endpoint,
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
+  
+config :phoenix, :generators,
+  migration: false,
+  model: false
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
