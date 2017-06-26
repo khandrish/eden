154cdd808e6a49403434e08f9d47fcfcfba1f021
Chris Hicks <khandrish@gmail.com>
2017-06-26 06:17:33 +0000

    added account app

---
+++ apps/exmud_account/lib/exmud_account.ex
@@ -0,0 +1,18 @@
+defmodule Exmud.Account do
+  @moduledoc """
+  Documentation for Exmud.Account.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exmud.Account.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/exmud_account/test/exmud_account_test.exs
@@ -0,0 +1,8 @@
+defmodule Exmud.AccountTest do
+  use ExUnit.Case
+  doctest Exmud.Account
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
