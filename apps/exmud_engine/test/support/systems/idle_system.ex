7bc1d4e660182e0ae7f3c3f712bc29e985fa9f72
Chris Hicks <mononym@users.noreply.github.com>
2018-03-03 07:39:10 +0000

    remove duplucate behavior usage

--- apps/exmud_engine/test/support/systems/idle_system.ex
+++ apps/exmud_engine/test/support/systems/idle_system.ex
@@ -2,8 +2,6 @@ defmodule Exmud.Engine.Test.System.Idle do
   @moduledoc """
   A barebones example of a system that idles after handling messages and running.
   """
-
-  @behaviour Exmud.Engine.System
   use Exmud.Engine.System
 
   def name, do: "Idle"
