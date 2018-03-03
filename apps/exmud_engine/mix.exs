34659df8290c27b8ae242210a35fbe0477cf4023
Chris Hicks <mononym@users.noreply.github.com>
2018-03-03 07:37:26 +0000

    update Elixir version

--- apps/exmud_engine/mix.exs
+++ apps/exmud_engine/mix.exs
@@ -7,7 +7,7 @@ defmodule Exmud.Engine.Mixfile do
       aliases: aliases(),
       app: :exmud_engine,
       deps: deps(),
-      elixir: "~> 1.5",
+      elixir: "~> 1.6.1",
       elixirc_paths: elixirc_paths(Mix.env),
       version: "0.1.0",
 
