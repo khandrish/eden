c5480b28526ecb687a472e8728a297269a8368c2
mononym <mononym@users.noreply.github.com>
2018-11-02 19:53:41 -0700

    remove extra supervisor

--- apps/engine/lib/engine/application.ex
+++ apps/engine/lib/engine/application.ex
@@ -4,16 +4,15 @@ defmodule Exmud.Engine.Application do
   import Exmud.Engine.Constants
   use Application
 
-  def start(_type, _args) do
+  def start( _type, _args ) do
     children = [
       Exmud.Engine.Repo,
-      {Registry, keys: :unique, name: system_registry()},
-      {Registry, keys: :unique, name: script_registry()},
-      {Registry, keys: :unique, name: player_registry()},
-      {DynamicSupervisor, strategy: :one_for_one, name: Exmud.Engine.CallbackSupervisor},
-      {DynamicSupervisor, strategy: :one_for_one, name: Exmud.Engine.CommandSupervisor}
+      { Registry, keys: :unique, name: system_registry() },
+      { Registry, keys: :unique, name: script_registry() },
+      { DynamicSupervisor, strategy: :one_for_one, name: Exmud.Engine.CallbackSupervisor },
+      { DynamicSupervisor, strategy: :one_for_one, name: Exmud.Engine.CommandSupervisor }
     ]
 
-    Supervisor.start_link(children, strategy: :one_for_one)
+    Supervisor.start_link( children, strategy: :one_for_one )
   end
 end
